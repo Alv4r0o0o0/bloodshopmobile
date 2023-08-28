632f7a6e95da4b7a9a2b8d3bb26c0dc9c21d348a
Spissini <jose.gangaspissini@gmail.com>
2023-08-28 17:31:16 -0400

    ñ

--- bloodshop/src/app/home/home.page.ts
+++ bloodshop/src/app/home/home.page.ts
@@ -6,7 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  loginForm: FormGroup; // Declarar el formulario FormGroup
+  
+  constructor(private formBuilder: FormBuilder) {
+    // Inicializa el formulario y agrega validaciones
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
 
-  constructor() {}
-
+  // Función para realizar la acción de inicio de sesión
+  login() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.value.email;
+      const password = this.loginForm.value.password;
+      // Aquí puedes realizar la lógica de inicio de sesión, por ejemplo, enviar los datos al servidor
+      console.log('Inicio de sesión exitoso', email, password);
+    }
+  }
 }
