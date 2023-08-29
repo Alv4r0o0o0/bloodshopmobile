2b1e44a78499a2c16eb0be56dcf3c2d1fca4095a
elxumare1 <118561281+elxumare1@users.noreply.github.com>
2023-08-29 03:48:33 -0400

    aaaa

--- bloodshop/src/app/pages/registro/registro.page.ts
+++ bloodshop/src/app/pages/registro/registro.page.ts
@@ -1,5 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+
+function rutValidator(control: AbstractControl): ValidationErrors | null {
+  const rut = control.value;
+
+  if (!rut) {
+    return null; // Si el campo está vacío, no hay error
+  }
+
+  const cleanRut = rut.replace(/[^0-9kK]/g, ''); // Remover caracteres no válidos
+
+  if (cleanRut.length !== 9) {
+    return { invalidLength: true }; // RUT debe tener 9 dígitos
+  }
+
+  const rutDigits = cleanRut.slice(0, -1);
+  const rutVerifier = cleanRut.slice(-1).toUpperCase();
+
+  const verifierCalc = (11 - (parseInt(rutDigits) % 11)) % 11;
+  const verifier = verifierCalc === 10 ? 'K' : verifierCalc.toString();
+
+  if (verifier !== rutVerifier) {
+    return { invalidRut: false };
+  }
+
+  return null; // RUT válido
+}
+
+
+
+
 
 @Component({
   selector: 'app-registro',
@@ -7,16 +37,25 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+
+  
   registroForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
     this.registroForm = this.formBuilder.group({
       nombre: ['', [Validators.required,]],
       apellido: ['', Validators.required],
+      rut: ['', [Validators.required, rutValidator]],
     });
   }
   ngOnInit() {
   }
-
+  onSubmit() {
+    if (this.registroForm) {
+      if (this.registroForm.valid) {
+        // El formulario es válido, realizar acción
+      }
+    }
+  }
 }
 
