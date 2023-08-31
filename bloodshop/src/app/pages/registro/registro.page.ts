81004f008a8ba51c2eac2d30b76fb1ee186c6ff9
elxumare1 <118561281+elxumare1@users.noreply.github.com>
2023-08-30 23:50:12 -0400

    aaaaa

--- bloodshop/src/app/pages/registro/registro.page.ts
+++ bloodshop/src/app/pages/registro/registro.page.ts
@@ -56,7 +56,7 @@ export class RegistroPage implements OnInit {
 pattern={
   nombre: /^(?=.*[A-Z])/,
   rut: /^\d{1,2}\.\d{3}\.\d{3}[-][0-9kK]{1}$/,
-  telefono: /^\d{10}$/,
+  telefono: /^\d{1,9}$/,
   correo: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
   contraseña: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
 
@@ -70,7 +70,7 @@ pattern={
       apellido: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(10), Validators.pattern(this.pattern.nombre)]],  
       rut: ['', [Validators.required, Validators.pattern(this.pattern.rut), validateRut]],
       fechnac: ['', [Validators.required, validateBirthDate]],
-      telefono: ['', [Validators.required, Validators.min(9), Validators.max(9),  validatePhoneNumber]],
+      telefono: ['', [Validators.required, Validators.pattern(this.pattern.telefono), validatePhoneNumber]],
       correo: ['', [Validators.required, Validators.pattern(this.pattern.correo)]],
       contraseña:['',[Validators.required, Validators.minLength(8), Validators.pattern(this.pattern.contraseña)]],
     });
