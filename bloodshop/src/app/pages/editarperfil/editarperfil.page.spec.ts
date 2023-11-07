b918244783f0273828a91f9f2095af7cccb18481
elxumare1 <118561281+elxumare1@users.noreply.github.com>
2023-11-07 16:16:41 -0300

    aaaa

--- bloodshop/src/app/pages/editarperfil/editarperfil.page.spec.ts
+++ bloodshop/src/app/pages/editarperfil/editarperfil.page.spec.ts
@@ -2,13 +2,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { EditarperfilPage } from './editarperfil.page';
 
+
 describe('EditarperfilPage', () => {
   let component: EditarperfilPage;
   let fixture: ComponentFixture<EditarperfilPage>;
 
   beforeEach(async() => {
+    describe('SomeComponent', () => {
+
+      const fakeActivatedRoute = {
+        snapshot: { data: { id="" , nombre="", } }} as ActivatedRoute;
+
     await TestBed.configureTestingModule({
-      providers: [ActivatedRoute]
+      providers: [ {provide: ActivatedRoute, useValue: fakeActivatedRoute} ]
     }).compileComponents();
     
     fixture = TestBed.createComponent(EditarperfilPage);
